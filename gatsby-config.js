88e0bb136aebd0bbb6c31bfb04fd7dd937355c8e
Timon <timonvanwaardhuizen@gmail.com>
2021-03-26 15:58:25 +0100

    layout changes and remove Instagram

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,14 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
+    // Default title of the page
     siteTitleAlt: `Technation`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Technation - Telepresence robot`,
+    // Used for SEO
+    siteDescription: `Technation improves the current telepresence Boki robot with sound.`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
   },
   plugins: [
     {
@@ -12,9 +19,10 @@ module.exports = {
       // See the theme's README for all available options
       options: {
         navigation: [
-          { name: `Projects`, slug: `/projects` },
-          { name: `Instagram`, slug: `/instagram` },
-          { name: `About`, slug: `/about` },
+          { name: `Concepten`, slug: `/concepten` },
+          { name: `Stappen`, slug: `/projects` },
+          { name: `Proces`, slug: `/proces` },
+          { name: `Team`, slug: `/team` },
         ],
       },
     },
@@ -22,12 +30,6 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-    },
-    {
-      resolve: `gatsby-source-instagram`,
-      options: {
-        username: `2315642426`,
       },
     },
     `gatsby-plugin-sitemap`,
